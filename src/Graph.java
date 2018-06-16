78eef0526d5d5aeba4c93d834437b02c86b726d2
Dennis Henle <dennis.henle@yahoo.de>
2018-06-16 12:40:13 +0200

    KOmmentare ohne Umlaute - Graph

--- src/Graph.java
+++ src/Graph.java
@@ -1,15 +1,15 @@
 // Gerichteter Graph.
-// (Ein ungerichteter Graph kann als gerichteter Graph reprÃ¤sentiert
+// (Ein ungerichteter Graph kann als gerichteter Graph repraesentiert
 // werden, bei dem jede Kante in beiden Richtungen vorhanden ist.)
 
 interface Graph {
-    // GrÃ¶ÃŸe des Graphen, d. h. Anzahl seiner Knoten.
-    // Die Knoten werden direkt durch Nummern zwischen 0 einschlieÃŸlich
-    // und size() ausschlieÃŸlich reprÃ¤sentiert, das heiÃŸt:
+    // Groesse des Graphen, d. h. Anzahl seiner Knoten.
+    // Die Knoten werden direkt durch Nummern zwischen 0 einschliesslich
+    // und size() ausschliesslich repraesentiert, das heisst:
     // Alle Parameter von Methoden dieser und anderer Schnittstellen,
-    // die Knoten bezeichnen (z. B. v), mÃ¼ssen Werte in diesem Bereich
-    // besitzen. (Dies muss nicht jedesmal Ã¼berprÃ¼ft werden.)
-    // Methoden, die Knoten als Resultat liefern (z. B. succ), mÃ¼ssen
+    // die Knoten bezeichnen (z. B. v), muessen Werte in diesem Bereich
+    // besitzen. (Dies muss nicht jedesmal ueberprueft werden.)
+    // Methoden, die Knoten als Resultat liefern (z. B. succ), muessen
     // Werte in diesem Bereich liefern.
     int size ();
 
@@ -18,12 +18,26 @@ interface Graph {
     int deg (int v);
 
     // i-ter direkter Nachfolger des Knotens v.
-    // i muss zwischen 0 einschlieÃŸlich und deg(v) ausschlieÃŸlich
-    // liegen. (Dies muss nicht Ã¼berprÃ¼ft werden.)
+    // i muss zwischen 0 einschliesslich und deg(v) ausschliesslich
+    // liegen. (Dies muss nicht ueberprueft werden.)
     int succ (int v, int i);
 
     // Transponierter Graph, d. h. ein neuer Graph mit denselben Knoten
-    // wie der aktuelle Graph, der fÃ¼r jede Kante (u, v) des aktuellen
-    // Graphen die entgegengesetzte Kante (v, u) enthÃ¤lt.
+    // wie der aktuelle Graph, der fuer jede Kante (u, v) des aktuellen
+    // Graphen die entgegengesetzte Kante (v, u) enthaelt.
     Graph transpose ();
 }
+
+/*
+ * Anmerkungen zu allen Algorithmen:
+ *
+ * Der uebergebene Graph g darf nicht null sein.
+ * Dies muss nicht ueberprueft werden.
+ *
+ * Die Nachfolger eines Knotens u muessen immer in der Reihenfolge
+ * g.succ(u, 0) bis g.succ(u, g.deg(u) - 1) durchlaufen werden.
+ */
+
+/*
+ * Algorithmen auf ungewichteten Graphen
+ */
--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -12,3 +12,17 @@ interface WeightedGraph extends Graph {
     // Wenn man fÃ¼r einen gewichteten Graphen transpose() aufruft,
     // erhÃ¤lt man einen Graphen des Typs Graph ohne Gewichte.
 }
+
+/*
+ * Anmerkungen zu allen Algorithmen:
+ *
+ * Der Ã¼bergebene Graph g darf nicht null sein.
+ * Dies muss nicht Ã¼berprÃ¼ft werden.
+ *
+ * Die Nachfolger eines Knotens u mÃ¼ssen immer in der Reihenfolge
+ * g.succ(u, 0) bis g.succ(u, g.deg(u) - 1) durchlaufen werden.
+ */
+
+/*
+ * Algorithmen auf ungewichteten Graphen
+ */
