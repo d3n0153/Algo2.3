2dd99bc5c81e34401d8ac78669c9a19f33544fa6
Dennis Henle <dennis.henle@yahoo.de>
2018-06-26 16:00:37 +0200

    letzter Stand Problem: PrioVergleich

--- src/MSTImpl.java
+++ src/MSTImpl.java
@@ -1,9 +1,36 @@
+import BinHeap;
 
 public class MSTImpl implements MST{
 
+	BinHeap heap = new BinHeap();
+	
 	@Override
 	public boolean compute(WeightedGraph g, int s) {
-		// TODO Auto-generated method stub
+		
+		// 1 Fuege alle Knoten außer s mit Prio UNENDLICH in eine minVorrangWarteschlange ein
+		for(int i = 0; i < g.size(); i++) {
+			if(i != s) {
+				heap.insert(i, Double.POSITIVE_INFINITY);
+			}
+		}
+		
+		// 2 setze u = s
+		int u = s;
+		
+		// 3 solange Warteschlange nicht leer ist
+		while(!heap.isEmpty()) {
+			
+			// 1 Für jeden Nachfolger von u:
+			for(int j = 0; j < g.deg(u); j++) {
+				int v = g.succ(u, j);
+				
+				// Wenn sich v in der Warteschlange befindet
+				// und das Gewicht w der Kante { u , v } kleiner als die 
+				// momentane Priorität von v ist:
+				if(heap.contains(v) && g.weight(v, j) < heap.minimum().prio()) {
+				}
+		}
+		
 		return false;
 	}
 
